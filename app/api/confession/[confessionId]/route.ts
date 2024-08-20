69cc5bf22537ef5da8bdd57a39bfeedd39ebd973
panfilo macantan <olifnapnatnacam27.com>
2024-08-20 17:15:49 +0800

    dynamic confessionId routes modified

--- app/api/confession/[confessionId]/route.ts
+++ app/api/confession/[confessionId]/route.ts
@@ -1,23 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
 import Confession from '@/lib/models/confessions.model';
 import { connectToDB } from '@/lib/mongoose';
-import { NextResponse } from 'next/server';
 
-export async function GET(request: { params: { confessionId: string } }) {
-	const { confessionId } = request.params;
+export async function GET(request: NextRequest) {
+    try {
+        const { searchParams } = new URL(request.url);
+        const confessionId = searchParams.get('confessionId');
 
-	try {
-		await connectToDB();
+        if (!confessionId) {
+            return NextResponse.json({ error: 'Confession ID is required' }, { status: 400 });
+        }
 
-		// Fetch the confession by ID
-		const confession = await Confession.findById({ _id: confessionId });
+        await connectToDB();
+        const confession = await Confession.findById(confessionId);
 
-		if (!confession) {
-			return NextResponse.json({ error: 'Confession not found!' }, { status: 404 });
-		}
+        if (!confession) {
+            return NextResponse.json({ error: 'Confession not found' }, { status: 404 });
+        }
 
-		return NextResponse.json(confession);
-	} catch (error) {
-		console.error('Failed to fetch confession:', error);
-		return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-	}
+        return NextResponse.json(confession);
+    } catch (error) {
+        console.error('Failed to fetch confession:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
 }
