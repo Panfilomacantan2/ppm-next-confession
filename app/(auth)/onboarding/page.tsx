69097f3eb7fcc57bd010f5e502ab1ddae8d55358
panfilo macantan <olifnapnatnacam27.com>
2024-08-25 01:01:54 +0800

    "Updated Clerk imports, removed Clerk React dependency, and reformatted middleware and package files"

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { currentUser } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation';
 import OnBoardingForm from '@/components/form/OnBoardingForm';
-
 const OnBoardingPage = async () => {
 	const user = await currentUser();
 
